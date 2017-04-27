360f961336c1e7e4da6574a9d48cea0bab8a3fcc
Evelyn Sharp <yihongshi90@gmail.com>
2017-04-27 14:18:18 -0600

    initial commit

---
+++ front.js
@@ -0,0 +1,20 @@
+// var button = document.getElementById('submitBtn');
+// button.addEventListener('click', function(e){
+//   e.preventDefault();
+//   console.log('click')
+//   saveContact();
+// });
+//
+// var contactArr = [];
+//
+// function saveContact() {
+//   var newContact = {};
+//   newContact.firstName = document.getElementById('firstName').value;
+//   newContact.lastName = document.getElementById('lastName').value;
+//   newContact.email = document.getElementById('emailAdd').value;
+//   newContact.message = document.getElementById('contactMessage').value;
+//   contactArr.push(newContact);
+//   console.log(contactArr);
+//   console.log(newContact);
+//
+// };
---
+++ index.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var fs = require('fs');
+var ejs = require('ejs');
+var app = express();
+var partials = require('express-partials');
+var expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static(__dirname + '/materialize/css'));
+app.use(express.static(__dirname + '/materialize/js'));
+app.use(express.static(__dirname + '/style'));
+app.use(express.static(__dirname + '/images'));
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/contact', function(request, response) {
+  response.sendFile(__dirname + '/contact.html');
+});
+
+
+
+app.get('/courses', function(request, response) {
+ fs.readFile('courses.json', 'utf8', function(err, data) {
+ var courses = JSON.parse(data);
+ response.locals = { courses: courses };
+ response.render('courses.ejs');
+ });
+});
+
+app.get('/courses/:id', function(request, response) {
+  fs.readFile('courses.json', 'utf8', function(err, data) {
+    var coursesParsed = JSON.parse(data);
+    var course = coursesParsed.filter( function(obj) {
+      return obj.id === parseInt(request.params.id);
+    })[0];
+
+    response.locals = { course: course };
+    response.render('course.ejs');
+ });
+});
+
+app.set('view engine', 'ejs');
+app.use(partials());
+
+app.get('/courses/:id/reviews', function(request, response) {
+  fs.readFile('reviews.json', 'utf8', function(err, data) {
+    var reviewsParsed = JSON.parse(data);
+    var reviews = reviewsParsed.filter( function(obj) {
+      return obj.id === parseInt(request.params.id);
+    })[0];
+
+    response.locals = { reviews: reviews };
+    response.render('reviews.ejs',{ title: '' , layout: "layout.ejs"});
+ });
+});
+
+
+
+app.get('/example',function(request,response) {
+    response.render('example.ejs', { title: '' , layout: "layout.ejs"});
+});
+
+
+// app.post('/contact', function(req, res) {
+//   var firstName = req.body.firstName;
+//   var lastName = req.body.lastName;
+//   var emailAdd = req.body.emailAdd;
+//   var contactMessage = req.body.contactMessage;
+//   var userInfo = {
+//     firstName:firstName;
+//     lastName:lastName;
+//     emailAdd:emailAdd;
+//     contactMessage:contactMessage;
+//   };
+//   var userInfoStr = JSON.stringify(userInfo);
+//   fs.writeFile('userinfo.json', userInfoStr , 'utf-8');
+//   console.log(userInfoStr);
+// });
+
+app.listen(8000);
+console.log('running in http://localhost:8000')
